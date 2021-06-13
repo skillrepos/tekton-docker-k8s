a6c8bcd44e3eafb066bdaa39bf609e75f64cbf9d
brentlaster <bcl@nclasters.org>
2021-06-13 09:08:32 -0400

    update data

--- docker-entrypoint-initdb.d/04-fill-table.sql
+++ docker-entrypoint-initdb.d/04-fill-table.sql
@@ -9,6 +9,8 @@ INSERT INTO `agents` VALUES (2,'Scooby','dog','1969-05-19','2000-02-11','fake gh
 INSERT INTO `agents` VALUES (3,'Perry','platypus','2013-01-20','2015-04-09','H. Doofensmirtz','...inator');
 INSERT INTO `agents` VALUES (4,'Mr. Krabs','crab','2010-06-17','2014-07-07','Plankton','various');
 INSERT INTO `agents` VALUES (5,'Bugs Bunny','rabbit','1966-05-22','1988-04-15','E. Fudd','wabbit gun');
+INSERT INTO `agents` VALUES (6,'Woody Woodpecker','bird','1959-05-22','1979-04-15','Buzz Buzzard','menacing stare');
+
 
 UNLOCK TABLES;
 
